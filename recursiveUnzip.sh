388a0b4b50b8b7a77dd95d3806e0b7a6988132ce
Jesse McGraw <jlmcgraw@gmail.com>
2015-06-03 11:30:12 -0400

    Still working on unzipping embedded zip files

--- recursiveUnzip.sh
+++ recursiveUnzip.sh
@@ -9,52 +9,55 @@ IFS="`printf '\n\t'`"  # Always put this in Bourne shell scripts
 #
 
 #BUG TODO: All zip files inside the original file are extracted in the same 
-#directory and not whatever subdirectory they were in
+#directory and not whatever subdirectory they were in in the base archive
 
 runzip()
 {
-   #
-   # Get parameters
-   #
+    #
+    # Get parameters
+    #
 
-   local zip_file=$1
-   local rm_flag=$2
+    local zip_file=$1
+    local rm_flag=$2
 
-   #If the target isn't all zip files then add a .zip extension to it
-   #which will get trimmed by basename
-   if [[ "${zip_file}" != *.zip ]]
-   then
-      zip_file=${zip_file}.zip
-   fi
+    #If the target isn't all zip files then add a .zip extension to it
+    #which will get trimmed by basename
+    if [[ "${zip_file}" != *.zip ]]
+        then
+            zip_file=${zip_file}.zip
+        fi
 
-   #
-   # Declare local variables
-   #
+    echo "zip_file: $zip_file"
 
-   local zip_dir
-   local zip_list
-   local new_zip_file
-   local new_unzip_error_code
+    #
+    # Declare local variables
+    #
 
-   #
-   # Verify thar zip file exists
-   #
+    local zip_dir
+    local zip_list
+    local new_zip_file
+    local new_unzip_error_code
 
-   if [[ ! -e ${zip_file} ]]
-    then
-        echo "$0 - Zip file not found : ${zip_file}" >&2
-        return 1
-    fi
+    #
+    # Verify that zip file exists
+    if [[ ! -e ${zip_file} ]]
+        then
+            echo "$0 - Zip file not found : ${zip_file}" >&2
+            return 1
+        fi
 
-   #
-   # Create unzip directory
-   #
+    #
+    # Create unzip directory
+    #
 
-   #Unzip to subdirectory nameed after file under current working directory
-   #removing the .zip suffix
-   zip_dir=$PWD/$(basename ${zip_file} .zip)
-   
-   if [ ! -d ${zip_dir} ]
+    #Unzip to subdirectory nameed after file under current working directory
+    #removing the .zip suffix
+#     zip_dir=$PWD/$(basename ${zip_file} .zip)
+    zip_dir=$(dirname ${zip_file})/$(basename ${zip_file} .zip)
+
+    echo "zip_dir: $zip_dir"
+
+    if [ ! -d ${zip_dir} ]
     then
         if ! mkdir ${zip_dir}
             then
@@ -63,62 +66,51 @@ runzip()
             fi
     fi
 
-   #
-   # Unzip in unzip directory
-   #
+    #
+    # Unzip in unzip directory
+    #
 
-   if ! unzip -q -o ${zip_file} -d ${zip_dir}
-    then
-        echo "$0 - Unzip error for file : $PWD/${zip_file}"
-        return 1
-    fi
-
-   #
-   # Get list of new zip files
-   #
-
-   #A file in the new subdirectory
-   new_zip_list=${zip_dir}/.new_zip_list
-   
-   #Find all *.zip files and save in new_zip_list
-   find ${zip_dir} -type f -name '*.zip' -print > ${new_zip_list}
-
-   #
-   # Recursive unzip of new zip files
-   #
-
-   unzip_error_code=0
-   #Save the current directory and change to where we just unzipped to
-   pushd ${zip_dir}
-   
-   #Read the list of zip files in zip_dir from  ${new_zip_list} and extract them
-   while read new_zip_file
-    do
-        if ! runzip ${new_zip_file} remove_zip
+    if ! unzip -qq ${zip_file} -d ${zip_dir}
         then
-            unzip_error_code=$?
-            break
+            echo "$0 - Unzip error for file : ${zip_dir}/${zip_file}"
+            return 1
         fi
-    done < ${new_zip_list}
-   
-   #Remove the list of zips file
-   rm -f ${new_zip_list}
-   
-   popd
 
-   #
-   # Remove zip file if required
-   #
+    #
+    # Recursive unzip of new zip files
+    #
 
-   if [ -n "${rm_flag}" -a ${unzip_error_code} -eq 0 ]
-   then
-      if ! rm ${zip_file}
-      then
-         echo "$0 - Failed to delete file : $PWD/${zip_file}"
-      fi
-   fi
+    unzip_error_code=0
+    #Save the current directory and change to where we just unzipped to
+#     pushd ${zip_dir}
 
-   return 0
+    #Read the list of zip files in zip_dir and extract them
+    #Using process substitution instead of a temp file for the find
+    #Note that there must be a space between the two < symbols to avoid confusion with the "here-doc" syntax of <<word. 
+    while read -r new_zip_file
+        do
+            if ! runzip ${new_zip_file} remove_zip
+                then
+                    unzip_error_code=$?
+                    break
+                fi
+        done < <(find ${zip_dir} -type f -name '*.zip' -print)
+
+#     popd
+
+    #
+    # Remove zip file if required
+    #
+echo "delete file : ${zip_dir}/${zip_file}"
+#     if [ -n "${rm_flag}" -a ${unzip_error_code} -eq 0 ]
+#         then
+#             if ! rm ${zip_file}
+#                 then
+#                     echo "$0 - Failed to delete file : ${zip_dir}/${zip_file}"
+#                 fi
+#         fi
+
+    return 0
 }
 
 runzip $1 $2
